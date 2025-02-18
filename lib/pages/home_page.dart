02e3637e420cb6bda54fffc1cdc2335ff96cc201
Harvin Chan <chantutharvinalberto14@gmail.com>
2025-02-18 12:41:34 -0600

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/mytab.dart';
+import 'package:flutter_application_1/tab/burger.dart';
+import 'package:flutter_application_1/tab/donut.dart';
+import 'package:flutter_application_1/tab/pizza.dart';
+import 'package:flutter_application_1/tab/smoothie.dart';
+import 'package:flutter_application_1/tab/pancakes.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = <Widget>[
+    const Mytab(iconPath: 'lib/icons/donut.png'),
+    const Mytab(iconPath: 'lib/icons/burger.png'),
+    const Mytab(iconPath: 'lib/icons/smoothie.png'),
+    const Mytab(iconPath: 'lib/icons/pancakes.png'),
+    const Mytab(iconPath: 'lib/icons/pizza.png'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Icon(
+            Icons.menu,
+            color: Colors.grey[800],
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            //1. Texto principal (text)
+            const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Text(
+                    "I want to",
+                    style: TextStyle(
+                      //Tamaño de la letra
+                      fontSize: 32,
+                      //Negritas
+                    ),
+                  ),
+                ),
+                Text(
+                  "Eat",
+                  style: TextStyle(
+                    //Tamaño de la letra
+                    fontSize: 32,
+                    //Negritas
+                    fontWeight: FontWeight.bold,
+                    //Subrayado
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ],
+            ),
+
+            //2. Pestañas (TabBar)
+            TabBar(tabs: myTabs),
+
+            //3. Contenido de pestañas (TabBarView)
+           const Expanded( // Aquí está la corrección
+              child: TabBarView(
+                children: [
+                  DonutTab(),
+                  BurgerTab(),
+                  SmoothieTab(),
+                  PancakesTab(),
+                  PizzaTab(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
